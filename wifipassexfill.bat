7376066ada2baf20c33463c44ceb29e3a7eea30f
sujal-2025325 <sujal.vcf@gmail.com>
2025-06-11 16:42:53 +0530

    Update wifipassexfill.bat

--- wifipassexfill.bat
+++ wifipassexfill.bat
@@ -1,5 +1,5 @@
 @echo off
-set "outFolder=%USERPROFILE%\Desktop\WiFiPasswords"
+set "outFolder=%USERPROFILE%\Downloads\WiFiPasswords"
 if not exist "%outFolder%" mkdir "%outFolder%"
 netsh wlan export profile key=clear folder="%outFolder%"
 echo Wi-Fi profiles exported to %outFolder%
